4455a2e81526d2c4ce48b4702b878121e1fba285
n1k0lay <kolia.d97@gmail.com>
2023-01-10 15:47:11 +0500

    Добавлена генерация данных sinh, cosh

--- api/genData.js
+++ api/genData.js
@@ -2,7 +2,10 @@ module.exports = () => {
     const data = {
         users: [],
         trigonometry: {
-            sin: []
+            sin: [],
+            cos: [],
+            sinh: [],
+            cosh: []
         }
     }
 
@@ -10,15 +13,56 @@ module.exports = () => {
 
     //sin от kt
     for (let k = 1; k <= 3; k++) {
-        data.trigonometry.sin[k-1] = {
+        data.trigonometry.sin[k - 1] = {
             k: k,
             data: []
         };
-        for (let i = 0; i < 3; i++) {
-            data.trigonometry.sin[k-1].data.push({t: i, value: Math.sin(k*i)})
+        for (let i = -20; i < 20; i++) {
+            data.trigonometry.sin[k - 1].data.push({t: i, value: Math.sin(k * i)})
         }
     }
 
+    //cos от kt
+    for (let k = 1; k <= 3; k++) {
+        data.trigonometry.cos[k - 1] = {
+            k: k,
+            data: []
+        };
+        for (let i = -20; i < 20; i++) {
+            data.trigonometry.cos[k - 1].data.push({t: i, value: Math.cos(k * i)})
+        }
+    }
+    //гиперболический синус sin от kt
+    for (let k = 1; k <= 3; k++) {
+        data.trigonometry.cos[k - 1] = {
+            k: k,
+            data: []
+        };
+        for (let i = -20; i < 20; i++) {
+            data.trigonometry.cos[k - 1].data.push({t: i, value: Math.sinh(k * i)})
+        }
+    }
+    //гиперболический косинус sin от kt
+    for (let k = 1; k <= 3; k++) {
+        data.trigonometry.sinh[k - 1] = {
+            k: k,
+            data: []
+        };
+        for (let i = -20; i < 20; i++) {
+            data.trigonometry.sinh[k - 1].data.push({t: i, value: Math.sinh(k * i)})
+        }
+    }
+
+    //гиперболический косинус cos от kt
+    for (let k = 1; k <= 3; k++) {
+        data.trigonometry.cosh[k - 1] = {
+            k: k,
+            data: []
+        };
+        for (let i = -20; i < 20; i++) {
+            data.trigonometry.cosh[k - 1].data.push({t: i, value: Math.cosh(k * i)})
+        }
+    }
 
     return data
 }
