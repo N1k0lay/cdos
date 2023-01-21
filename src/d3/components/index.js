449d8291b1acddc3bed2efd65a67ed7f10fe6b36
n1k0lay <kolia.d97@gmail.com>
2023-01-21 12:00:45 +0500

    D3: Улучшение производительности с помощью деления MultilineChart.js на независимые компоненты. Каждая компонента имеет свой жизненный цикл.

--- src/d3/components/index.js
+++ src/d3/components/index.js
@@ -1,4 +1,5 @@
-export { default as Line } from "./Line";
+export { default as Area } from "./Area";
 export { default as Axis } from "./Axis";
 export { default as GridLine } from "./GridLine";
-export { default as Area } from "./Area";
+export { default as Legend } from "./Legend";
+export { default as Line } from "./Line";
