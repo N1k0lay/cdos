c0c163d8c33a7dde23140cb40b59d89c4a9deba9
n1k0lay <kolia.d97@gmail.com>
2023-01-24 22:47:31 +0500

    Добавлено получение и адаптация данных для linechart

--- src/App.js
+++ src/App.js
@@ -5,33 +5,55 @@ import vcit from "../src/d3/dataTest/VCIT.json";
 import portfolio from "../src/d3/dataTest/portfolio.json";
 import MultilineChart from "./d3/MultilineChart/MultilineChart";
 import {Legend} from "./d3/components";
+import LineChart from "./d3/LineChart";
 
 
 function App() {
 
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
+    const [dataLine, setDataLine] = useState({});
 
     const portfolioData = {
         name: "Portfolio",
         color: "#ffffff",
-        items: portfolio.map((d) => ({ ...d, date: new Date(d.date) }))
+        items: portfolio.map((d) => ({...d, date: new Date(d.date)}))
     };
     const schcData = {
         name: "SCHC",
         color: "#d53e4f",
-        items: schc.map((d) => ({ ...d, date: new Date(d.date) }))
+        items: schc.map((d) => ({...d, date: new Date(d.date)}))
     };
     const vcitData = {
         name: "VCIT",
         color: "#5e4fa2",
-        items: vcit.map((d) => ({ ...d, date: new Date(d.date) }))
+        items: vcit.map((d) => ({...d, date: new Date(d.date)}))
     };
+
+    useEffect(() => {
+        if (isLoaded) {
+            console.log(data)
+            console.log(data[0])
+            const res = data[0];
+            let viewData = [];
+            for (let i = 0; i < res.k.length; i++) {
+                viewData[i] = {k: res.k[i], data: []};
+                for (let j = 0; j < res.data.length; j++) {
+                    viewData[i].data.push({time: res.data[j].t, value: res.data[j].value[i]})
+                }
+                console.log(res.k[i])
+            }
+            console.log(viewData)
+        }
+    }, [data, isLoaded])
+
+    console.log(vcitData)
+
     const dimensions = {
         width: 600,
         height: 300,
-        margin: { top: 30, right: 30, bottom: 30, left: 60 }
+        margin: {top: 30, right: 30, bottom: 30, left: 60}
     };
 
     const [selectedItems, setSelectedItems] = useState([]);
@@ -40,6 +62,7 @@ function App() {
         portfolioData,
         ...[schcData, vcitData].filter((d) => selectedItems.includes(d.name))
     ];
+
     const onChangeSelection = (name) => {
         const newSelectedItems = selectedItems.includes(name)
             ? selectedItems.filter((item) => item !== name)
@@ -48,7 +71,7 @@ function App() {
     };
 
     useEffect(() => {
-        fetch("http://localhost:3003/db")
+        fetch("http://localhost:3003/trigonometry/")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -61,6 +84,8 @@ function App() {
                 }
             )
     }, [])
+
+
     if (error) {
         return <div>Ошибка: {error.message}</div>;
     } else if (!isLoaded) {
@@ -68,12 +93,16 @@ function App() {
     } else {
         return (
             <div className="App">
-                <Legend
-                    data={legendData}
-                    selectedItems={selectedItems}
-                    onChange={onChangeSelection}
-                />
-                <MultilineChart data={chartData} dimensions={dimensions} />
+                <LineChart
+                    data={[portfolioData, schcData, vcitData]}
+                    dimensions={dimensions}/>
+
+                {/*<Legend*/}
+                {/*    data={legendData}*/}
+                {/*    selectedItems={selectedItems}*/}
+                {/*    onChange={onChangeSelection}*/}
+                {/*/>*/}
+                {/*<MultilineChart data={chartData} />*/}
             </div>
         );
     }
