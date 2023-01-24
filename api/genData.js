72bc791c27e2c09f9a8cc34b2ef485e40f4be1c4
n1k0lay <kolia.d97@gmail.com>
2023-01-24 22:47:08 +0500

    Правка генерации данных

--- api/genData.js
+++ api/genData.js
@@ -1,41 +1,41 @@
-function sin(data) {
+function sin() {
     //sin от kt
     //Создание объекта. к - коэффициенты, data - рассчитанные данные
-    data.trigonometry.sin = {
-        k: [], data: [],
+    sin = {
+        name: 'Синус',
+        k: [],
+        data: [],
     }
     //Генерация и запись коэффициентов в объект k
     for (let k = 1; k <= 3; k++) {
-        data.trigonometry.sin.k = [...data.trigonometry.sin.k, k];
+        sin.k = [...sin.k, k*2];
     }
     //получение коэффициентов из объекта
-    let odds = data.trigonometry.sin.k;
+    let odds = sin.k;
     //Расчет и запись данных
     for (let i = 0; i < 20; i++) {
         //Создание структуры
-        data.trigonometry.sin.data[i] = {
-            t: '', value: {}
-        }
+        sin.data.push({
+            t: '', value: []
+        })
         //Указание временной точки
-        data.trigonometry.sin.data[i].t = i;
+        sin.data[i].t = i;
 
         //Заполнение рассчитанными данными
         //используем спред оператор, чтобы не перезаписывать старые данные, а дополнять их
         for (let k = 0; k < odds.length; k++) {
-            data.trigonometry.sin.data[i].value = {
-                ...data.trigonometry.sin.data[i].value,
-                [odds[k]]: Math.sin(odds[k] * i)
-            }
+            sin.data[i].value.push(Math.sin(odds[k] * i))
         }
 
     }
-    return data.trigonometry.sin
+    return sin
 }
 
 function cos() {
     //cos от kt
     //Создание объекта. к - коэффициенты, data - рассчитанные данные
     cos = {
+        name: 'Консинус',
         k: [], data: [],
     }
     //Генерация и запись коэффициентов в объект k
@@ -69,14 +69,12 @@ function cos() {
 
 module.exports = () => {
     const data = {
-        menu: [], trigonometry: {
-            sin: [], cos: [], sinh: [], cosh: []
-        }
+        menu: [], trigonometry: []
     }
 
     //Тригонометрия
-    data.trigonometry.sin = sin(data);
-    data.trigonometry.cos = cos();
+    data.trigonometry.push(sin());
+    data.trigonometry.push(cos());
 
     return data
 }
