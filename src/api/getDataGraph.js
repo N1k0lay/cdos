09740b487d66033d2332ef6eafd8a835dc858f72
n1k0lay <kolia.d97@gmail.com>
2023-04-17 20:00:29 +0500

    Удалены лишние npm пакеты

--- src/api/getDataGraph.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function getDataGraph(link) {
-    return axios
-        .get(`http://localhost:3003/${link}`)
-        .then(res => res.data)
-        .catch(error => error)
-}
